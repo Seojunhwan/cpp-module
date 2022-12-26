6e6dc020f32ca723d52a72001999cf59a1d19e7f
Seojunhwan <seojunhwan@kakao.com>
2022-12-26 17:32:12 +0900

    style: reindent ClapTrap.hpp

--- module03/ex00/ClapTrap.hpp
+++ module03/ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: junseo <junseo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 13:08:21 by junseo            #+#    #+#             */
-/*   Updated: 2022/12/26 17:10:06 by junseo           ###   ########.fr       */
+/*   Updated: 2022/12/26 17:31:27 by junseo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,23 @@
 
 class ClapTrap
 {
-public:
-	ClapTrap(void);
-	ClapTrap(const std::string name);
-	ClapTrap(const ClapTrap &obj);
-	~ClapTrap();
+	public:
+		ClapTrap(void);
+		ClapTrap(const std::string name);
+		ClapTrap(const ClapTrap &obj);
+		~ClapTrap();
 
-	ClapTrap &operator=(const ClapTrap &ref);
+		ClapTrap &operator=(const ClapTrap &ref);
 
-	void attack(const std::string &target);
-	void takeDamage(unsigned int amount);
-	void beRepaired(unsigned int amount);
+		void attack(const std::string &target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
 
-private:
-	std::string _name;
-	unsigned int _hit_points;
-	unsigned int _energy;
-	unsigned int _attack_damage;
+	private:
+		std::string _name;
+		unsigned int _hit_points;
+		unsigned int _energy;
+		unsigned int _attack_damage;
 };
 
 #endif
