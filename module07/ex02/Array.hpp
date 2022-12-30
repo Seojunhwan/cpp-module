6430bc61c983411f6a1ea28d813176c13b8a2689
kjy00 <jjuni28@naver.com>
2022-12-30 16:06:51 +0900

    module07 ex02
    Co-authored-by: Seojunhwan <seojunhwan@kakao.com>

---
+++ module07/ex02/Array.hpp
@@ -0,0 +1,56 @@
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+# include <iostream>
+
+template <typename T>
+class Array
+{
+    private:
+        T               *_arr;
+        unsigned int    _size;
+    public:
+        Array() : _arr(new T[0]), _size(0) {}
+        Array(unsigned int n) : _arr(new T[n]), _size(n) {}
+        Array(const Array &obj) : _arr(new T[obj.size]), _size(obj._size) {
+            for (unsigned int i = 0; i < this->_size; i++) {
+                this->_arr[i] = obj._arr[i];
+            }
+        }
+
+        Array& operator=(const Array &obj) {
+            if (this != &obj) {
+                delete[] this->_arr;
+                this->_arr = new T[obj._size];
+                this->_size = obj._size;
+                for (unsigned int i = 0; i < this->_size; i++) {
+                    this->_arr[i] = obj._arr[i];
+                }
+            }
+            return *this;
+        }
+
+        ~Array() {
+            delete[] this->_arr;
+        }
+
+        T& operator[](unsigned int index) {
+            if (index > this->_size) {
+                throw std::out_of_range("index is out bounds");
+            }
+            return this->_arr[index];
+        }
+
+        const T& operator[](unsigned int index) const {
+            if (index > this->_size) {
+                throw std::out_of_range("index is out bounds");
+            }
+            return this->_arr[index];
+        }
+
+        unsigned int size() const {
+            return this->_size;
+        };
+};
+
+#endif
